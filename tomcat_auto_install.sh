7372dbc748c2e1dc79685e674e12dca1e7b10329
root <root@ip-172-31-2-21.ap-southeast-2.compute.internal>
2017-09-21 11:12:14 +0000

    tomcat service edited

--- tomcat_auto_install.sh
+++ tomcat_auto_install.sh
@@ -22,7 +22,7 @@ sudo chmod -R g+r conf
 sudo chmod g+x conf
 sudo chown -R tomcat webapps/ work/ temp/ logs/
 sudo chown tomcat /usr/local/tomcat
-sudo cp ~/tomcat_install_automatically/tomcat100.service /etc/systemd/system/tomcat.service
+sudo cp /tomcat_install_automatically/tomcat100.service /etc/systemd/system/tomcat.service
 
 sudo systemctl daemon-reload
 sudo systemctl start tomcat
