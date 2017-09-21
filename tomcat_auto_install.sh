b9f42caae20454e6cf0b0d2ddff77775a30857db
root <root@ip-172-31-2-21.ap-southeast-2.compute.internal>
2017-09-21 10:35:33 +0000

    tomcat with jenkins auto test

--- tomcat_auto_install.sh
+++ tomcat_auto_install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sudo yum install wget vim git -y
+sudo yum install wget vim  -y
 sudo yum install java-1.8.0-openjdk -y
 
 cd /usr/local/
 sudo wget http://www-eu.apache.org/dist/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz
 sudo tar -xvzf apache-tomcat-8.5.20.tar.gz
 sudo mv  apache-tomcat-8.5.20  tomcat
+
+sudo wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war -P /usr/local/tomcat/webapps
 
 sudo groupadd tomcat
 sudo useradd -M -s /bin/nologin -g tomcat -d /usr/local/tomcat tomcat
@@ -19,7 +21,7 @@ cd /usr/local/tomcat
 sudo chmod -R g+r conf
 sudo chmod g+x conf
 sudo chown -R tomcat webapps/ work/ temp/ logs/
-
+sudo chown tomcat /usr/local/tomcat
 sudo cp ~/tomcat_install_automatically/tomcat100.service /etc/systemd/system/tomcat.service
 
 sudo systemctl daemon-reload
